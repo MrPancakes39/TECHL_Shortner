dee65942d97c18ec6d1d272d6f552fa1b1d92782
MrPancakes39 <selenahasan39@proton.me>
2023-02-27 19:47:36 +0200

    Init an express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on http://127.0.0.1:${PORT}.`));
+
+app.get("/", (req, res) => {
+    res.send("Hello Shortner");
+});
