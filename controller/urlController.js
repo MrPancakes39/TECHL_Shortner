dad3fd7a31fdb56cbbe62ee14dbc4619409f6ee8
MrPancakes39 <selenahasan39@proton.me>
2023-03-04 14:13:52 +0200

    Added nanoid for default parameter

--- controller/urlController.js
+++ controller/urlController.js
@@ -1,9 +1,13 @@
 const Redirect = require("../models/urlModel");
 const { z } = require("zod");
+const nanoid = require("nanoid");
 
 const RequestSchema = z
     .object({
-        code: z.string().min(1),
+        code: z
+            .string()
+            .min(1)
+            .default(() => nanoid(12)),
         original_url: z
             .string()
             .url()
@@ -25,25 +29,26 @@ module.exports.createRedirect = async function (req, res) {
         });
         return res.status(400).send({ errors });
     }
+    console.log(result.data);
     const { code, original_url } = result.data;
 
-    const safe_code = code
-        .toString()
-        .trim()
-        .replace(/[^a-z0-9]/gi, "_");
+    // const safe_code = code
+    //     .toString()
+    //     .trim()
+    //     .replace(/[^a-z0-9]/gi, "_");
 
-    const redirect_in_db = await Redirect.findOne({ short_code: safe_code });
-    if (redirect_in_db) {
-        return res.status(400).json({ message: `Code ${safe_code} is already taken.` });
-    }
+    // const redirect_in_db = await Redirect.findOne({ short_code: safe_code });
+    // if (redirect_in_db) {
+    //     return res.status(400).json({ message: `Code ${safe_code} is already taken.` });
+    // }
 
-    await Redirect.create(
-        {
-            short_code: safe_code,
-            destination_url: original_url,
-        },
-        (err) => (err ? res.status(500).json({ message: err.message }) : null)
-    );
+    // await Redirect.create(
+    //     {
+    //         short_code: safe_code,
+    //         destination_url: original_url,
+    //     },
+    //     (err) => (err ? res.status(500).json({ message: err.message }) : null)
+    // );
 
     return res.sendStatus(200);
 };
