7a0dc3a9f7e0b9c46a73724a331f83f9c59bac4d
MrPancakes39 <selenahasan39@proton.me>
2023-03-04 15:20:25 +0200

    Updated frontend to work with new API

--- public/main.js
+++ public/main.js
@@ -5,13 +5,13 @@ const result = document.querySelector("#result");
 
 submit.addEventListener("click", async (event) => {
     event.preventDefault();
-    const data = {
-        shortjson: {
-            code: codeElt.value,
-            original_url: urlElt.value,
-        },
-    };
-    const code = data.shortjson.code;
+    const code = codeElt.value;
+    const original_url = urlElt.value;
+
+    const data = {};
+    if (code) data["code"] = code;
+    if (original_url) data["original_url"] = original_url;
+
     const res = await fetch("/api/create", {
         method: "POST",
         body: JSON.stringify(data),
@@ -19,14 +19,31 @@ submit.addEventListener("click", async (event) => {
             "Content-Type": "application/json",
         },
     });
-    const URL = `${location.href}api/redirect/${code}`;
     if (res.ok) {
-        result.innerHTML = `You can view it at <a href="${URL}" target="_blank">${URL}</a>`;
+        const URL = (await res.json()).short_url;
+        result.innerHTML = `You can view it at <a href="${URL}" target="_blank">${URL}</a>.`;
     } else {
-        const msg = await res.json();
-        result.innerHTML = msg?.message ?? "Something went wrong";
-        if (msg.message.includes("taken")) {
-            result.innerHTML += `<br>View it here: <a href="${URL}" target="_blank">${URL}</a>`;
+        const { errors } = await res.json();
+        let msg;
+        switch (errors.type) {
+            case "message":
+                msg = errors.message;
+                break;
+            case "invalid-format":
+                msg = "Errors in the following fields:\n";
+                for (let key in errors) {
+                    if (key !== "type") {
+                        msg += `${key}:\n    ${errors[key].join(",\n    ")},\n`;
+                    }
+                }
+                msg = msg.slice(0, -2); // removes last ",\n"
+                console.log(errors);
+                console.log({ msg });
+                break;
+            default:
+                msg = "Something went wrong";
+                break;
         }
+        result.innerHTML = msg;
     }
 });
